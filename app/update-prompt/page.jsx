2f8a2b74184a26510d92de6b42baa6cec4670e53
vipoool <31506872+vipoool@users.noreply.github.com>
2025-05-08 10:37:46 +0530

    added suspense

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useState, Suspense } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 
 import Form from "@components/Form";
@@ -9,6 +9,7 @@ const EditPrompt = () => {
 	const router = useRouter();
 	const searchParams = useSearchParams();
 	const promptId = searchParams.get("id");
+
 	const [submitting, setSubmitting] = useState(false);
 	const [post, setPost] = useState({
 		prompt: "",
@@ -54,13 +55,15 @@ const EditPrompt = () => {
 	};
 
 	return (
-		<Form
-			type="Edit"
-			post={post}
-			setPost={setPost}
-			submitting={submitting}
-			handleSubmit={updatePrompt}
-		/>
+		<Suspense fallback={<div>Loading...</div>}>
+			<Form
+				type="Edit"
+				post={post}
+				setPost={setPost}
+				submitting={submitting}
+				handleSubmit={updatePrompt}
+			/>
+		</Suspense>
 	);
 };
 
